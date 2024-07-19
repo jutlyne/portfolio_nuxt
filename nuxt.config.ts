e867762a57d108f459dd1e7b537fa45dc940b8ff
kyvc <kyvc.nta@gmail.com>
2024-07-19 08:21:35 +0700

    update ssr for home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@ant-design-vue/nuxt', 'nuxt-booster', 'nuxt-particles', "@nuxt/image"],
+  ssr: false,
+  routeRules: {
+    '/': { ssr: false }
+  },
+  modules: ['@ant-design-vue/nuxt', 'nuxt-booster', 'nuxt-particles', '@nuxt/image'],
   booster: {
     detection: {
       performance: true,
